d8e1451a5c360099054415ef272c5925b5d0a838
wb-hx510875 <wb-hx510875@alibaba-inc.com>
2020-01-22 10:54:50 +0800

    work: improve tests use await/promise for alamofire request. improve codecov.yml add tests add tests mock method

--- Tests/TeaTests/TeaTests.swift
+++ Tests/TeaTests/TeaTests.swift
@@ -87,10 +87,112 @@ final class TeaTests: XCTestCase {
         XCTAssertEqual("owner", model.owner)
     }
 
+    func testTeaCoreSleep() {
+        let sleep: Int = 10
+        let start: Double = Date().timeIntervalSince1970
+        TeaCore.sleep(sleep)
+        let end: Double = Date().timeIntervalSince1970
+        XCTAssertTrue(Int((end - start)) >= sleep)
+    }
+
+    func testTeaCoreDoAction() {
+        var res: TeaResponse?
+        let expectation = XCTestExpectation(description: "Test async request")
+        let request: requestCompletion = { done in
+            let request_ = TeaRequest()
+            request_.protocol_ = "http"
+            request_.method = "POST"
+            request_.pathname = "/v2/drive/list"
+
+            request_.headers = [
+                "user-agent": "Tea Test for TeaCore.doAction",
+                "host": "sz16.api.alicloudccp.com",
+                "content-type": "application/json; charset=utf-8"
+            ]
+            let utils: TestsUtils = TestsUtils()
+            request_.headers["date"] = utils._getRFC2616Date()
+            request_.headers["accept"] = "application/json"
+            request_.headers["x-acs-signature-method"] = "HMAC-SHA1"
+            request_.headers["x-acs-signature-version"] = "1.0"
+            request_.headers["authorization"] = "acs AccessKeyId:TestSignature"
+
+            let model = ListDriveRequestModel()
+            model.owner = "owner"
+            request_.body = utils._toJSONString([model])
+
+            var runtime = [String: Any]()
+            runtime["connectTimeout"] = 0
+            runtime["readTimeout"] = 0
+            res = TeaCore.doAction(request_, runtime)
+            done(res)
+        }
+        let done: doneCompletion = { response in
+            // do something when get response
+            res = response as? TeaResponse
+            XCTAssertNotNil(res)
+            XCTAssertNil(res?.error)
+            XCTAssertEqual("403", res?.statusCode ?? "0")
+            XCTAssertEqual("{\"code\":\"InvalidParameter.Accesskeyid\",\"message\":\"The input parameter AccessKeyId is not valid. DoesNotExist\"}", String(bytes: res!.data, encoding: .utf8))
+            expectation.fulfill()
+        }
+
+        TeaClient.async(request: request, done: done)
+
+        wait(for: [expectation], timeout: 100.0)
+    }
+
+    func testTeaCoreAllowRetry() {
+        var result: Bool = TeaCore.allowRetry(nil, 3)
+        XCTAssertFalse(result)
+
+        var dict: [String: Int] = [String: Int]()
+        dict["maxAttempts"] = 5
+        result = TeaCore.allowRetry(dict, 0)
+        XCTAssertTrue(result)
+    }
+
+    func testTeaCoreGetBackoffTime() {
+        var dict: [String: String] = [String: String]()
+        dict["policy"] = "no"
+        XCTAssertEqual(0, TeaCore.getBackoffTime(dict, 3))
+
+        dict["policy"] = "yes"
+        dict["period"] = ""
+        XCTAssertEqual(0, TeaCore.getBackoffTime(dict, 3))
+
+        dict["period"] = "-1"
+        XCTAssertEqual(3, TeaCore.getBackoffTime(dict, 3))
+
+        dict["period"] = "1"
+        XCTAssertEqual(1, TeaCore.getBackoffTime(dict, 3))
+    }
+
+    func testTeaCoreIsRetryable() {
+        XCTAssertTrue(TeaCore.isRetryable(TeaException.ValidateError("foo")))
+    }
+
+    func testTeaConverterMerge() {
+        var dict1: [String: String] = [String: String]()
+        var dict2: [String: String] = [String: String]()
+
+        dict1["foo"] = "bar"
+        dict2["bar"] = "foo"
+
+        let dict: [String: String] = TeaConverter.merge(dict1, dict2) as! [String: String]
+        XCTAssertEqual(dict["foo"], "bar")
+        XCTAssertEqual(dict["bar"], "foo")
+    }
+
     static var allTests = [
         ("testTeaCoreComposeUrl", testTeaCoreComposeUrl),
         ("testTeaModelToMap", testTeaModelToMap),
         ("testTeaModelValidate", testTeaModelValidate),
         ("testTeaModelToModel", testTeaModelToModel),
+        ("testTeaCoreSleep", testTeaCoreSleep),
+        ("testTeaCoreDoAction", testTeaCoreDoAction),
+        ("testTeaCoreAllowRetry", testTeaCoreAllowRetry),
+        ("testTeaCoreGetBackoffTime", testTeaCoreGetBackoffTime),
+        ("testTeaCoreIsRetryable", testTeaCoreIsRetryable),
+        ("testTeaConverterMerge", testTeaConverterMerge),
     ]
 }
